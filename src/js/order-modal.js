5a3b0486191121090edab67b16224848d1342535
anna_borysova <anna@pavlyuk.org>
2025-12-15 20:35:11 +0200

    add js

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -1,35 +1,49 @@
-// import axios from "axios";
+import axios from "axios";
+import iziToast from "izitoast";
 
-// const formElem = document.querySelector("#order-modal-form");
+const formElem = document.querySelector("#order-modal-form");
 
-// formElem.addEventListener ("submit", async e => {
-//   e.preventDefault();
-//   const { email, phone } = e.target.elements;
-//   const formData = {
-//     email: email.value,
-//     phone: phone.value,
-//     animalId: ${animalId},
-//     comment: 
+formElem.addEventListener ("submit", async e => {
+  e.preventDefault();
+  const { email, phone } = e.target.elements;
+  const formData = {
+    name: userName.value,
+    phone: phone.value,
+    animalId: `${animalId}`,
+    comment: `${message.value}`,
+  }
+  try {const response = axios.post ("https://paw-hut.b.goit.study/api/orders", formData);
 
-//   }
-// });
+const orderData = response.data;
+console.log(orderData);
+iziToast.success({
+    title: "Success", 
+    message: `Ви знайшли собі найкращого друга!`,
+    position: `topLeft`,
+});
+e.target.reset();
+} catch (error) {
+    iziToast.error({
+        title: "Error", 
+        message: `В цей раз ви не змогли знайти друга. Спробуйте ще раз.`,
+        position: `topLeft`,
+    });
+};
+});
 
 
-// (() => {
-//   const refs = {
-//     // Додати атрибут data-modal-open на кнопку відкриття
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     // Додати атрибут data-modal-close на кнопку закриття
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     // Додати атрибут data-modal на бекдроп модалки
-//     modal: document.querySelector("[data-modal]"),
-//   };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-//   function toggleModal() {
-//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-//     refs.modal.classList.toggle("is-open");
-//   }
-// })();
+  function toggleModal() {
+    
+    refs.modal.classList.toggle("is-open");
+  }
+})();

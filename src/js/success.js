a2b02db7874523cdf35e8790caa91b9f43e307cd
Uffaser <gerasimenkom405@gmail.com>
2025-12-16 20:20:24 +0200

    Refactor success animation logic to improve performance and readability

--- src/js/success.js
+++ src/js/success.js
@@ -129,11 +129,15 @@ const total = 30;
 for (let i = 0; i < total; i++) {
     const paw = document.createElement('div');
     paw.classList.add('paw');
+
     paw.style.animationDelay = `${(total - i) * 0.25}s`;
+
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.classList.add('icon');
+
     const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
     use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#paw');
+
     svg.appendChild(use);
     paw.appendChild(svg);
     successAnim.appendChild(paw);

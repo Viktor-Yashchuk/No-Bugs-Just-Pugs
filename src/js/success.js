08d24b3f4cf7a4921ac906f61baf60b42887574e
Uffaser <gerasimenkom405@gmail.com>
2025-12-22 01:58:37 +0200

    fix

--- src/js/success.js
+++ src/js/success.js
@@ -4,7 +4,6 @@ import Swiper from 'swiper';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import Raty from 'raty-js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
